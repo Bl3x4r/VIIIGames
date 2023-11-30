78fb917b24f0e699e45da8355cea16b48e3cfee5
Bl3x4r <llanosb65@gmail.com>
2023-11-30 20:20:15 -0300

    se actualiza el server, para mostrar las paginas

---
+++ app/server.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app_flask import app
+
+@app.route("/")
+def index():
+    return render_template("test_login.html")
+
+@app.route("/crear/cuenta")
+def cuenta():
+    return render_template("test_creacion.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
