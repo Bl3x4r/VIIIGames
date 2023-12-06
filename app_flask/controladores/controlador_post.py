b343dfec1201c01f1671dd0b1720f84770c95575
alonsoznova <alonsoznova@gmail.com>
2023-12-06 00:09:42 -0300

    función para crear posts

--- app_flask/controladores/controlador_post.py
+++ app_flask/controladores/controlador_post.py
@@ -1,4 +1,20 @@
 from flask import render_template, session, redirect, request, flash
-from app_flask.modelos.modelo_usuario import Usuario
+from app_flask.modelos.modelo_post import Post
 from app_flask import app
-from flask_bcrypt import Bcrypt
+
+@app.route('/crear/post', methods=['GET'])
+def formulario_post():
+    if 'id_usuario' not in session:
+        return redirect('/')
+    return render_template('crear_post.html')
+
+@app.route('/procesar/creacion_post', methods=['POST'])
+def crear_post():
+    nuevo_post= {
+        **request.form,
+        'usuario_id': session['id_usuario']
+    }
+    Post.crear_post(nuevo_post)
+    return redirect('/inicio')
+
+    
