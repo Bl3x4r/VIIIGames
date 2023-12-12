16afa427f149fe9983825fb52f3015160d19b0eb
alonsoznova <alonsoznova@gmail.com>
2023-12-11 22:40:32 -0300

    mejoras en dashboard y varios

--- app_flask/config/mysqlconnection.py
+++ app_flask/config/mysqlconnection.py
@@ -3,7 +3,7 @@ class MySQLConnection:
     def __init__(self, db):
         connection = pymysql.connect(host = 'localhost',
                                     user = 'root', 
-                                    password = '@B2G3Z6K8benja', 
+                                    password = 'root', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
